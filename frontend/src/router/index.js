f0b075e45740f6d757c6f2ba4ee536fd89c3df3c
windscroll <2321864583@qq.com>
2026-05-27 23:43:13 +0800

    Delete frontend directory

--- frontend/src/router/index.js
+++
@@ -1,4 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-const routes = [{ path: '/', component: Home }]
-export default createRouter({ history: createWebHistory(), routes })
